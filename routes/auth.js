95060159fbb5f172dff97877f7890211eb55466f
shettyrohan183 <76105128+shettyrohan183@users.noreply.github.com>
2022-02-15 09:52:08 +0530

    Password Hashing

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const User = require("../modules/User");
 const router = express.Router();
-const { body, validationResult } = require("express-validator");
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Harryisagoodcoder';
 
 // create a user using :POST "/api/auth/""
 // creating user no authencation no login
@@ -27,13 +31,22 @@ router.post(
       if (user) {
         return res.status(400).json({ error: "Email already exit " });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      // create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-
-      res.json({ user });
+      const data = {
+        user:{
+          id:user.id
+        }
+      }
+      const authtoken =   jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("some error occured");
