3724679bcce6be43237c54fd28f87ad5c8e4ba0b
shettyrohan183 <76105128+shettyrohan183@users.noreply.github.com>
2022-02-16 12:35:12 +0530

    deletenotes

--- routes/notes.js
+++ routes/notes.js
@@ -15,11 +15,16 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   }
 });
 
-
 // route 2 : add the notes using post
-router.post("/addnotes",fetchuser,[
+router.post(
+  "/addnotes",
+  fetchuser,
+  [
     body("title", "Enter Valid title").isLength({ min: 3 }),
-    body("description", "description must be at least 5 character").isLength({min: 5,}),],
+    body("description", "description must be at least 5 character").isLength({
+      min: 5,
+    }),
+  ],
   async (req, res) => {
     try {
       const { title, description, tag } = req.body;
@@ -33,16 +38,74 @@ router.post("/addnotes",fetchuser,[
         description,
         tag,
         user: req.user.id,
-      })
+      });
       const savedNote = await notes.save();
       res.json(savedNote);
     } catch (error) {
-        console.log(error.message);
+      console.log(error.message);
       res.status(500).send("some error occured");
     }
   }
 );
 
+// route  3: update the notes using put
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
 
+  try {
+    // create a newnote object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    // find the note to be updated and update
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      res.status(404).send("Not Found");
+    }
+    if (note.user.toString() != req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json({ note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("some error occured");
+  }
+});
+
+// route  4: delete the notes using delete
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+ 
+  try {
+    // find the note to be deleted and delete
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      res.status(404).send("Not Found");
+    }
+    // allow delteion only if user owns the notes
+    if (note.user.toString() != req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Notes.findByIdAndDelete(req.params.id);
+    res.json({ Success: "deleted Sucessfully", note: note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("some error occured");
+  }
+});
 
 module.exports = router;
